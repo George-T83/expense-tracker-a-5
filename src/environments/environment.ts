9a7d88f1391ea1ea02fb45b8a66f67db6ca6ce74
George Tannious <george.tanious@gmail.com>
2026-04-16 22:03:29 -0500

    chore: add firebase configuration to environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAG3CWzj7B89vfFcRlRmvF_JFH6Sovzv7U',
+    authDomain: 'expense-tracker-a-5.firebaseapp.com',
+    projectId: 'expense-tracker-a-5',
+    storageBucket: 'expense-tracker-a-5.firebasestorage.app',
+    messagingSenderId: '667828347084',
+    appId: '1:667828347084:web:cdec1a3d74e2b3b9392269',
+  },
+};
